4dbe6c5bc0850f2f56faa0a210fa294ace9f80be
cjwsstrm <cjwesstrom@gmail.com>
2017-09-11 20:42:14 +0000

    created file http-functions and copied contents from print_callbacks.js

---
+++ http-functions.js
@@ -0,0 +1,32 @@
+module.exports = function getHTML (options, callback) {
+  let https = require('https');
+
+  function getHTML (options, callback) {
+    https.get(options, function (response) {
+    // set encoding of received data to UTF-8
+      response.setEncoding('utf8');
+      let bufferedData = '';
+      response.on('data', (data) => {
+        console.log('Chunk Received. Length:', data.length);
+        bufferedData += data;
+      });
+
+      response.on('end', () => {
+        console.log('Response stream complete.');
+        callback(bufferedData);
+      });
+    });
+  }
+
+  function printHTML (html) {
+    console.log(html);
+  }
+}
+
+
+// getHTML(requestOptions, printHTML);
+//
+// let requestOptions = {
+//   host: 'sytantris.github.io',
+//   path: '/http-examples/step4.html'
+// };
